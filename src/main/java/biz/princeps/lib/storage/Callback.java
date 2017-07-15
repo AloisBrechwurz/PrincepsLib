9feb5aad0472c692b80861e46e694f7af5ed7881
SpatiumPrinceps <spatium@princeps.biz>
2017-07-15 15:56:40 +0200

    Added throws sqle

--- src/main/java/biz/princeps/lib/storage/Callback.java
+++ src/main/java/biz/princeps/lib/storage/Callback.java
@@ -1,11 +1,12 @@
 package biz.princeps.lib.storage;
 
 import java.sql.ResultSet;
+import java.sql.SQLException;
 
 /**
  * Created by spatium on 15.07.17.
  */
 public interface Callback {
 
-    void runAfter(ResultSet resultSet);
+    void runAfter(ResultSet resultSet) throws SQLException;
 }
